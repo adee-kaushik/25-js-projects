3d083ad361af3ed57717c2d37012bf032fc96bcc
Aditya mishra <adityaamishra6002@gmail.com>
2026-06-16 17:40:29 +0530

    commented codes removed

--- Project_2/script.js
+++ Project_2/script.js
@@ -1,24 +1,3 @@
-// let body = document.querySelector("body");
-// let hex_code = document.querySelector("#hex-code");
-// let btn = document.querySelector(".btn");
-
-// let hex_color= ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
-
-
-
-
-//     btn.addEventListener('click', function(){
-//         let color= '';
-//         for(i=0;i<6; i++){
-//     let randomindex= Math.floor(Math.random() * hex_color.length);
-//     color += hex_color[randomindex];
-//        body.style.backgroundColor = '#'+ color
-//           hex_code.textContent = '#'+ color
-//     }
-       
-       
-//     })
-
 
 let body = document.querySelector("body");
 let hex_code = document.querySelector("#hex-code");
